14c8c2f87370582df88cc3fb3ccba0bef3d1efd0
Tin Nguyen <nguyentritin211@gmail.com>
2023-08-07 15:57:49 +0300

    4.1-4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const BlogsRouter = require('./controllers/Blogs')
+const config = require('./utils/config')
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs',BlogsRouter)
+
+module.exports = app
